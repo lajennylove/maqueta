d0ad651e003efd8489e8ae40fd9d10b75c68c24f
lajennylove <lajennylove@users.noreply.github.com>
2020-02-20 02:13:28 -0600

    Final Commit
    Add the requeried files.

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es-MX">
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Main CSS Style -->
+    <link rel="stylesheet" href="dist/css/style.css">
+    
+    <!-- Site Icon -->
+    <link rel="icon" type="image/png" href="dist/images/logo.png" />
+    
+    <!-- Title -->
+	<title>Boilerplate</title>
+	
+</head>
+<body>
+
+    <!-- Main Container -->
+    <div class="container">
+		<div class="row mt-3">
+	        <div class="col-md-1"></div>
+	        <div class="col-md-10">
+                <h1>Hello World</h1>
+                <p>Boilerplate of Gulp + Sass + Bootstrap + Uglify.</p>
+	        </div>
+	        <div class="col-md-1"></div>
+	    </div>
+	</div>
+	<!-- Main Container END -->
+		
+</body>
+</html>
